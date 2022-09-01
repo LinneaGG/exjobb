e06fc887af978a22c26f34a3a2984a90aa55b577
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-01 11:14:46 +0200

    Update grapetree_nj.sh

--- grapetree_nj.sh
+++ grapetree_nj.sh
@@ -2,4 +2,4 @@ module load bioinfo-tools
 module load python
 # pip install grapetree
 
-python /domus/h1/linne/.local/lib/python3.9/site-packages/grapetree/grapetree.py -p core.aln -m NJ > treefile.txt
+python /path/to/grapetree/grapetree.py -p core.aln -m NJ > treefile.txt
