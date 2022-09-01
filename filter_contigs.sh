d2d2b6a57e4504e7e5fe12bb3b3b39f9649011a5
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-01 11:13:50 +0200

    Update filter_contigs.sh

--- filter_contigs.sh
+++ filter_contigs.sh
@@ -1,8 +1,8 @@
-for f in /crex/proj/snic2021-23-717/private/exjobb/assembly/human/*/contigs.fasta
+for f in /path/to/assemblies/*/contigs.fasta
 do
 
         id=$(basename $(dirname $f))
 
-        awk 'BEGIN{FS="_";x=0;};{if ($4>=500 && $6>=1 && $0~/>/) x=1; else if ($0~/>/) x=0; if (x==1) print $0;}' ${f} > /crex/proj/snic2021-23-717/private/exjobb/filtered_assembly/human/${id}_filtered.fasta
+        awk 'BEGIN{FS="_";x=0;};{if ($4>=500 && $6>=1 && $0~/>/) x=1; else if ($0~/>/) x=0; if (x==1) print $0;}' ${f} > /path/to/filtered_assemblies_outdir/${id}_filtered.fasta
 
 done
