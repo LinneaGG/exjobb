5bff2967f60363e7398e27781ae2e5547c661d42
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-01 13:44:51 +0200

    Update filter_contigs.sh

--- filter_contigs.sh
+++ filter_contigs.sh
@@ -1,8 +1,11 @@
-for f in /path/to/assemblies/*/contigs.fasta
+path_to_assemblies="/path/to/assemblies"
+path_to_filtered_assembly_outdir="/path/to/filtered_assembly_outdir"
+
+for f in ${path_to_assemblies}/*/contigs.fasta
 do
 
         id=$(basename $(dirname $f))
 
-        awk 'BEGIN{FS="_";x=0;};{if ($4>=500 && $6>=1 && $0~/>/) x=1; else if ($0~/>/) x=0; if (x==1) print $0;}' ${f} > /path/to/filtered_assemblies_outdir/${id}_filtered.fasta
+        awk 'BEGIN{FS="_";x=0;};{if ($4>=500 && $6>=1 && $0~/>/) x=1; else if ($0~/>/) x=0; if (x==1) print $0;}' ${f} > ${path_to_filtered_assembly_outdir}/${id}_filtered.fasta
 
 done
