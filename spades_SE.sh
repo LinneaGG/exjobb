ca7e5ca1330b512e2e301d33fd73c87a1cd9c98c
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-01 11:07:42 +0200

    Update spades_SE.sh

--- spades_SE.sh
+++ spades_SE.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 8
 #SBATCH -t 30:00:00
-#SBATCH -J spades_human
+#SBATCH -J spades_SE
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user linnea.gauffingood.6719@student.uu.se
 
@@ -16,7 +16,7 @@ fi
 
 regex="[^_]*"
 
-for i in /crex/proj/snic2021-23-717/private/exjobb/trimmed/human_O157_klad8/*.fastq.gz
+for i in /path/to/trimmed_reads/*.fastq.gz
 do
 
 	basename=$(basename $i)
@@ -25,12 +25,12 @@ do
 		basename=${BASH_REMATCH[*]}
 	fi
 
-        if [[ ! -d /crex/proj/snic2021-23-717/private/exjobb/assembly/human/$basename ]]
+        if [[ ! -d /path/to/assembly_outdir/$basename ]]
         then
-                mkdir /crex/proj/snic2021-23-717/private/exjobb/assembly/human/$basename
+                mkdir /path/to/assembly_outdir/$basename #Create directory for each isolate in assembly outdir
         fi
 
-        echo "spades.py -s $i -m 50 --careful -o /crex/proj/snic2021-23-717/private/exjobb/assembly/human/$basename" > spades_script_human.sh
+        echo "spades.py -s $i -m 50 --careful -o /path/to/assembly_outdir/$basename" > spades_script_human.sh
         bash spades_script.sh
 
 done
