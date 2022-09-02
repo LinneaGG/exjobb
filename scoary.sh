5abe7b6881f6970352fafbf63d9334bdf4e5fef5
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-02 08:57:19 +0200

    Update scoary.sh

--- scoary.sh
+++ scoary.sh
@@ -2,4 +2,4 @@ module load bioinfo-tools
 module load python
 #pip install scoary
 
-/home/linne/.local/bin/scoary -o /crex/proj/snic2021-23-717/private/scoary -g /crex/proj/snic2021-23-717/private/roary/gene_presence_absence.csv -t /crex/proj/snic2021-23-717/private/pipelines/traits.csv
+/path/to/scoary -o /path/to/scoary_outdir -g /path/to/roary_outdir/gene_presence_absence.csv -t /path/to/traits.csv
