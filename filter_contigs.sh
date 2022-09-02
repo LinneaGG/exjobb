431252f84c809f043cc2e14c0836d668b29a4f93
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2022-09-02 09:48:46 +0200

    Update filter_contigs.sh

--- filter_contigs.sh
+++ filter_contigs.sh
@@ -1,9 +1,10 @@
+#Removes contigs with length < 500 bp and k-mer coverage < 1
+
 path_to_assemblies="/path/to/assemblies"
 path_to_filtered_assembly_outdir="/path/to/filtered_assembly_outdir"
 
 for f in ${path_to_assemblies}/*/contigs.fasta
 do
-
         id=$(basename $(dirname $f))
 
         awk 'BEGIN{FS="_";x=0;};{if ($4>=500 && $6>=1 && $0~/>/) x=1; else if ($0~/>/) x=0; if (x==1) print $0;}' ${f} > ${path_to_filtered_assembly_outdir}/${id}_filtered.fasta
